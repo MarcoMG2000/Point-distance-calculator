cedf84a776dcaac81f9612fd27ac9b9bf2123565
MarcoMG2000 <lostrenate12@gmail.com>
2023-03-31 17:51:26 +0200

    Versión inicial del proyecto

---
+++ AlgoritmosAvanzadosP3/src/main/java/model/Punto.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author lostr
+ */
+public class Punto extends Point implements Comparable {
+
+    @Override
+    public int compareTo(Object o) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
