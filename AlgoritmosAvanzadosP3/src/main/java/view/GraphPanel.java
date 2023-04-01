5a6a1df77a77727cf686dc2b541c28372d837d72
jotaefe95 <81982789+juanfco95@users.noreply.github.com>
2023-04-01 13:09:37 +0200

    TAREA J

--- AlgoritmosAvanzadosP3/src/main/java/view/GraphPanel.java
+++ AlgoritmosAvanzadosP3/src/main/java/view/GraphPanel.java
@@ -1,18 +1,55 @@
 /**
  * Practica 3 Algoritmos Avanzados - Ing Informática UIB
+ *
  * @date 23/04/2023
  * @author jfher, JordiSM, peremarc, MarcoMG
- * @url 
+ * @url
  */
 package view;
 
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.geom.Ellipse2D;
+import static java.lang.Math.random;
+import java.util.Random;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.LineBorder;
 
 /**
  * Panel para pintar los puntos generados.
  */
 public class GraphPanel extends JPanel {
-    
+
     private View vista;
-    
+    private int margenLat = 160;
+    private int margenVert = 50;
+
+    public GraphPanel(View v) {
+        vista = v;
+        Border borde = new LineBorder(Color.BLACK, 2);
+        setBorder(borde);
+        setLayout(null);
+        setBounds(margenLat, margenVert,
+                vista.getWidth() - (margenLat * 2 + 20),
+                vista.getHeight() - (margenVert * 3));
+        setBackground(Color.WHITE);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2d = (Graphics2D) g;
+        /*Aqui tendria que ir una llamada al modelo para hacer un get de los
+        valores generados para los puntos y luego ya seria ir haciendo llamadas
+        a la funcion drawPoint con los valores de cada punto*/
+    }
+
+    public void drawPoint(Graphics2D g2d, int x, int y) {
+        Ellipse2D.Double circle = new Ellipse2D.Double(x, y, 10, 10);
+        g2d.setColor(Color.RED);
+        g2d.fill(circle);
+    }
 }
--- AlgoritmosAvanzadosP3/src/main/java/view/View.java
+++ AlgoritmosAvanzadosP3/src/main/java/view/View.java
@@ -1,26 +1,31 @@
 /**
  * Practica 3 Algoritmos Avanzados - Ing Informática UIB
+ *
  * @date 23/04/2023
  * @author jfher, JordiSM, peremarc, MarcoMG
- * @url 
+ * @url
  */
 package view;
 
 import model.Model;
 import controller.Controller;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GradientPaint;
+import java.awt.Toolkit;
+import java.util.Random;
 import javax.swing.JFrame;
 
-
 /**
- * Vista de la aplicación, aquí interactuaremos con la aplicación y 
+ * Vista de la aplicación, aquí interactuaremos con la aplicación y
  * visualizaremos todos los datos y los resultados de las operaciónes.
  */
 public class View extends JFrame {
-    
+
     // PUNTEROS DEL PATRÓN MVC
     private Controller controlador;
     private Model modelo;
-    
+
     // CONSTRUCTORS
     public View() {
     }
@@ -29,19 +34,26 @@ public class View extends JFrame {
         this.controlador = controlador;
         this.modelo = modelo;
     }
-    
+
     // CLASS METHODS
     /**
      * Clase que inicializa la ventana principal y añade todos los elementos al
      * JFrame.
      */
     public void mostrar() {
-        this.setSize(800,600);
+        setLayout(null);
+        Dimension pantalla = Toolkit.getDefaultToolkit().getScreenSize();
+        int ancho = 850;
+        int alto = 650;
+        int x = (pantalla.width - ancho) / 2;
+        int y = (pantalla.height - alto) / 2;
+        setBounds(x, y, ancho, alto);
+        GraphPanel panel = new GraphPanel(this);
+        this.add(panel);
         this.setVisible(true);
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        // TODO: implementar método.
     }
-    
+
     // GETTERS & SETTERS
     public Controller getControlador() {
         return controlador;
@@ -57,6 +69,6 @@ public class View extends JFrame {
 
     public void setModelo(Model modelo) {
         this.modelo = modelo;
-    } 
-    
+    }
+
 }
