3895bc49a6903182e9c5596217fdd774f6b9449e
Noe <Noe@DESKTOP-F77OGPH.home>
2023-04-02 14:23:29 +0200

    Limpieza de código

--- AlgoritmosAvanzadosP3/src/main/java/algoritmosavanzadosp3/AlgoritmosAvanzadosP3.java
+++ AlgoritmosAvanzadosP3/src/main/java/algoritmosavanzadosp3/AlgoritmosAvanzadosP3.java
@@ -1,8 +1,9 @@
 /**
  * Practica 3 Algoritmos Avanzados - Ing Informática UIB
+ *
  * @date 23/04/2023
  * @author jfher, JordiSM, peremarc, MarcoMG
- * @url 
+ * @url
  */
 package algoritmosavanzadosp3;
 
@@ -17,28 +18,27 @@ public class AlgoritmosAvanzadosP3 {
 
     public static void main(String[] args) {
         MVCInit();
-        System.out.println("Hello World");
     }
-    
+
     /**
-     * Establece los punteros entre las distintas clases del patrón MVC para
-     * que se puedan comunicar entre ellas.
+     * Establece los punteros entre las distintas clases del patrón MVC para que
+     * se puedan comunicar entre ellas.
      */
     private static void MVCInit() {
         Model modelo = new Model();
         View vista = new View();
         Controller controlador = new Controller();
-        
+
         modelo.setVista(vista);
         modelo.setControlador(controlador);
-        
+
         vista.setModelo(modelo);
         vista.setControlador(controlador);
-        
+
         controlador.setModelo(modelo);
         controlador.setVista(vista);
-        
+
         vista.mostrar();
     }
-    
+
 }
