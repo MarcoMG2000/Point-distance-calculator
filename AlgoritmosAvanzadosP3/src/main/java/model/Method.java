3a4f09ffc061e60fe820b323c81c5e84fc73c66e
MarcoMG2000 <lostrenate12@gmail.com>
2023-04-02 14:25:58 +0200

    Adición del enum Method

---
+++ AlgoritmosAvanzadosP3/src/main/java/model/Method.java
@@ -0,0 +1,15 @@
+/**
+ * Practica 3 Algoritmos Avanzados - Ing Informática UIB
+ *
+ * @date 23/04/2023
+ * @author jfher, JordiSM, peremarc, MarcoMG
+ * @url
+ */
+package model;
+
+/**
+ * Enum para los dos métodos de resolver el problema de los puntos cercanos.
+ */
+public enum Method {
+    FUERZA_BRUTA, DIVIDE_Y_VENCERAS
+}
