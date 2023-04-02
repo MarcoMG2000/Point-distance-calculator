7d836b94af289c5ec18e300a28f86d9faf752268
MarcoMG2000 <lostrenate12@gmail.com>
2023-04-02 14:24:54 +0200

    Limpieza de código

---
+++ AlgoritmosAvanzadosP3/src/main/java/model/Distribution.java
@@ -0,0 +1,15 @@
+/**
+ * Practica 3 Algoritmos Avanzados - Ing Informática UIB
+ *
+ * @date 23/04/2023
+ * @author jfher, JordiSM, peremarc, MarcoMG
+ * @url
+ */
+package model;
+
+/**
+ * Enum de las distintas distribuciones para generar los puntos aleatoriamente.
+ */
+public enum Distribution {
+    GAUSSIAN, CHI2
+}
